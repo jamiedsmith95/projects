f16d214e4eef302b96d4c8f1fb91528f0cb0abd1
jamiedsmith95 <jamie.d.smith95@gmail.com>
2023-12-07 23:47:58 +0000

    samles neatly ordered

--- frontend/javascript/web/js/display.js
+++ frontend/javascript/web/js/display.js
@@ -1,18 +1,28 @@
-function createRow(container,studentName,samples){
+function createRow(container,userName,samples){
   const row=document.createElement("div");
   row.classList.add("row");
   container.appendChild(row);
 
   const rowLabel=document.createElement("div");
-  rowLabel.innerHTML=studentName;
+  rowLabel.innerHTML=userName;
   rowLabel.classList.add("rowLabel");
   row.appendChild(rowLabel);
 
   for(let sample of samples){
     const {id,label}=sample;
+
+    const sampleContainer=document.createElement("div");
+    sampleContainer.id="sample_"+id;
+    sampleContainer.classList.add("sampleContainer")
+
+    const sampleLabel = document.createElement("div")
+    sampleLabel.id="sampleLabel"
+    sampleLabel.innerHTML=label;
+    sampleContainer.appendChild(sampleLabel);
     const img=document.createElement('img');
     img.src=constants.IMG_DIR+'/'+id+'.png';
     img.classList.add("thumb");
-    row.appendChild(img);
+    sampleContainer.appendChild(img);
+    row.appendChild(sampleContainer);
   }
 }
