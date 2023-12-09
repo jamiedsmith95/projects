4306d357e28c8851b5770cb80d33921be8b254b3
jamiedsmith95 <jamie.d.smith95@gmail.com>
2023-12-09 00:56:51 +0000

    more on drawing, JS CSS

--- frontend/javascript/common/utils.js
+++ frontend/javascript/common/utils.js
@@ -4,9 +4,20 @@ utils.formatPercent=(n)=>{
   return (n*100).toFixed(2)+"%";
 }
 
+utils.styles={
+  car:{color:'gray',text:'🚗'},
+  fish:{color:'red',text:'🐠'},
+  house:{color:'yellow',text:'🏠'},
+  tree:{color:'green',text:'🌳'},
+  bicycle:{color:'cyan',text:'🚲'},
+  guitar:{color:'blue',text:'🎸'},
+  pencil:{color:'magenta',text:'✏'},
+  clock:{color:'lightgray',text:'🕔'}
+}
+
 utils.printProgress=(count,max)=>{
   process.stdout.clearLine();
-  process.stdout.cursorTo(0);
+  process.stdout.cursorTo();
   const percent=utils.formatPercent(
     count/max
   );
--- frontend/javascript/web/js/display.js
+++ frontend/javascript/web/js/display.js
@@ -1,28 +1,69 @@
-function createRow(container,userName,samples){
-  const row=document.createElement("div");
+function createRow(container, userName, samples) {
+  const row = document.createElement("div");
   row.classList.add("row");
   container.appendChild(row);
 
-  const rowLabel=document.createElement("div");
-  rowLabel.innerHTML=userName;
+  const rowLabel = document.createElement("div");
+  rowLabel.innerHTML = userName;
   rowLabel.classList.add("rowLabel");
   row.appendChild(rowLabel);
 
-  for(let sample of samples){
-    const {id,label}=sample;
+  for (let sample of samples) {
+    const { id, label } = sample;
 
-    const sampleContainer=document.createElement("div");
-    sampleContainer.id="sample_"+id;
+    const sampleContainer = document.createElement("div");
+    sampleContainer.id = "sample_" + id;
+    sampleContainer.onclick = () => {
+      handleClick(sample, false);
+    }
+    sampleContainer.onmouseover = () =>
+      handleHover(sample)
     sampleContainer.classList.add("sampleContainer")
 
     const sampleLabel = document.createElement("div")
-    sampleLabel.id="sampleLabel"
-    sampleLabel.innerHTML=label;
+    sampleLabel.id = "sampleLabel"
+    sampleLabel.innerHTML = label;
     sampleContainer.appendChild(sampleLabel);
-    const img=document.createElement('img');
-    img.src=constants.IMG_DIR+'/'+id+'.png';
+    const img = document.createElement('img');
+    img.src = constants.IMG_DIR + '/' + id + '.png';
     img.classList.add("thumb");
     sampleContainer.appendChild(img);
     row.appendChild(sampleContainer);
+  }
+}
+
+function handleHover(sample) {
+  if (sample == null) {
+    [...document.querySelectorAll('.hovered')].forEach((e) => e.classList.remove('hovered'));
+
+    return
+  }
+  [...document.querySelectorAll('.hovered')].forEach((e) => e.classList.remove('hovered'));
+  const el = document.getElementById("sample_" + sample.id);
+  if (el.classList.contains('emphasize')) {
+    el.parentElement.classList.add('hovered');
+    el.classList.add('hovered');
+    el.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    });
+  };
+}
+
+function handleClick(sample, doScroll = true) {
+  if (!sample) {
+    [...document.querySelectorAll('.emphasize')].forEach((e) => e.classList.remove('emphasize'));
+  } else {
+    [...document.querySelectorAll('.emphasize')].forEach((e) => e.classList.remove('emphasize'));
+
+    const el = document.getElementById("sample_" + sample.id);
+    el.classList.add('emphasize');
+    if (doScroll) {
+      el.scrollIntoView({
+        behavior: 'auto',
+        block: 'center'
+      });
+    }
+    chart.selectSample(sample);
   }
 }
